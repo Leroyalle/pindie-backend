6dd79769cd0e9504b193ce780bbdd8e502b6e878
Leroyalle <nikolajmelonov@yandex.ru>
2024-05-29 16:47:04 +0300

    fix cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,19 +1,18 @@
-const allowedCors = ['https://play-pindie.nomorepartiesco.ru'];
+const allowedCors = ['https://play-pindie.nomorepartiesco.ru/'];
 
-function cors(req, res, next) {
-  const { origin } = req.headers;
+const cors = (request, response, next) => {
+  const { origin } = request.headers;
 
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    response.header('Access-Control-Allow-Origin', origin);
+    response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    response.header(
+      'Access-Control-Allow-Headers',
+      'Content-type,Authorization,Accept,X-Custom-Header',
+    );
   }
 
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization',
-  );
-
   next();
-}
+};
 
 module.exports = cors;
